e847fe5f9c0954412ca835299fdcd0970d0f0a4b
haliphax <haliphax@gmail.com>
2021-04-10 22:43:23 -0500

    refactor: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stream-avatars
+
+An attempt to make a very basic HTML/JS/CSS Twitch stream avatars overlay.
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// TODO pull list of chatters on an interval
+// TODO create/remove avatars
+// TODO animate avatars
