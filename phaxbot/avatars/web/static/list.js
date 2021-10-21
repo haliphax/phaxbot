94dd02ff9844efabae16a516cab7d761496f2d51
haliphax <haliphax@gmail.com>
2021-10-21 12:48:29 -0500

    feature: add "copy command" button to avatars list

--- phaxbot/avatars/web/static/list.js
+++ phaxbot/avatars/web/static/list.js
@@ -3,6 +3,41 @@ import { store } from './avatars.js';
 
 'use strict';
 
+/** length of time in milliseconds to show "Copied!" text */
+const FLASH_TIMEOUT = 2 * 1000;
+
+Vue.component('copy-button', {
+	data() {
+		return { enabled: true };
+	},
+	computed: {
+		copiedText() {
+			return this.copied || 'Copied!';
+		},
+	},
+	methods: {
+		copy(avatar) {
+			navigator.clipboard.writeText(this.value);
+			this.enabled = false;
+			setTimeout(
+				(function() { this.enabled = true; }).bind(this),
+				FLASH_TIMEOUT);
+		},
+	},
+	props: ['copied', 'text', 'value'],
+	template: /*html*/`
+		<button type="button" class="btn btn-sm btn-dark"
+			:disabled="!enabled" @click="copy">
+			<span v-if="enabled">
+				{{ text }}
+			</span>
+			<span v-if="!enabled">
+				{{ copiedText }}
+			</span>
+		</button>
+	`,
+});
+
 Vue.component('avatars-list', {
 	data() {
 		return {
@@ -19,7 +54,7 @@ Vue.component('avatars-list', {
 		},
 	},
 	async created() {
-		await fetch(this.avatarsUrl || '/api/avatars').then(r => r.json())
+		await fetch(this.avatarsUrl || '/avatars/api/list').then(r => r.json())
 			.then(async d => {
 				if (d.hasOwnProperty('excludeList'))
 					this.exclude = d.excludeList;
@@ -27,20 +62,23 @@ Vue.component('avatars-list', {
 				await this.$store.dispatch('json', d);
 			});
 	},
-	template: `
+	template: /*html*/`
 		<div>
 			<slot>
 				<div class="jumbotron">
 					<h1 class="display-4">Available avatars</h1>
 					<p class="lead">
-						These avatars are available to represent you on-stream.
-						Use the <code>!avatar</code> command to select one from
-						the list.
+						These avatars are available to represent you on-stream. Use the
+						<code>!avatar</code> command to select one from the list.
 					</p>
 					<p>
-						If you do not wish to be represented by an avatar
-						on-stream, use <code>!avatar hide</code> to prevent
-						your username from being processed.
+						If you do not wish to be represented by an avatar on-stream, use
+						<code>!avatar hide</code> to prevent your username from being
+						processed.
+					</p>
+					<p>
+						Use the <em>Copy command</em> button beside each avatar to copy the
+						necessary Twitch chat command to your clipboard.
 					</p>
 				</div>
 			</slot>
@@ -60,6 +98,8 @@ Vue.component('avatars-list', {
 									{{ role }}
 								</span>
 							</small>
+							<copy-button :value="'!avatar ' + avatar" text="Copy command">
+							</copy-button>
 						</th>
 						<td class="text-left">
 							<component :is="'avatar-' + avatar"></component>
